b64790641a74b448135577c7cec93a5adeaed03d
doraghter <demid.daria@yandex.by>
2023-03-18 23:42:31 +0300

    black jack

---
+++ black_jack/black_jack.py
@@ -0,0 +1,57 @@
+import cards
+import games
+
+
+class BJCard(cards.Card):
+    ACE_VALUE = 1
+
+    @property
+    def value(self):
+        if self.is_face_up:
+            v = BJCard.RANKS.index(self.rank) + 1
+            if v > 10:
+                v = 10
+            else:
+                v = None
+            return v
+
+
+class BJDesk(cards.Deck):
+    def populate(self):
+        for suit in BJCard.SUITS:
+            for rank in BJCard.RANKS:
+                self.cards.append(BJCard(rank, suit))
+
+
+class BJHand(cards.Hand):
+    def __init__(self, name):
+        super(BJHand, self).__init__()
+        self.name = name
+
+    def __str__(self):
+        rep = self.name + ":\t" + super(BJHand, self).__str__()
+        if self.total:
+            rep += "(" + str(self.total) + ")"
+        return rep
+
+    @property
+    def total(self):
+        for card in self.cards:
+            if not card.value:
+                return None
+        t = 0
+        for card in self.cards:
+            t += card.value
+        contains_ace = False
+        for card in self.cards:
+            if card.value == BJCard.ACE_VALUE:
+                contains_ace =True
+        if contains_ace and t <= 11:
+            t += 10
+        return t
+
+    def is_busted(self):
+        return self.total > 21
+
+# class BJPlayer(BJHand):
+#     def
